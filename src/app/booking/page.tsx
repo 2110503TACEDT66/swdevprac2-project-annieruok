2565a5c28dc36acc74f8f68f5c3460eedd2d93d1
Chayanin <33849student1@gmail.com>
2024-03-25 19:05:09 +0700

    make function booking limit 3 and  bokking between 10-13 may 2022

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -5,20 +5,26 @@ import { Dayjs } from "dayjs"
 import { useSearchParams } from "next/navigation"
 import { useState } from "react"
 import { useDispatch } from "react-redux"
-import { AppDispatch } from "@/redux/store"
+import { AppDispatch,useAppSelector } from "@/redux/store"
 import { addBooking } from "@/redux/features/bookSlice"
+import BookingList from "@/components/BookingList"
+import dayjs from "dayjs"
 
 export default function Booking(){
+  const startDate = dayjs('2022-05-9 17:00:00');
+  const endDate = dayjs('2022-05-13 23:59:59');
+
+  const hospitalItems = useAppSelector((state) => state.bookSlice.bookItems);
     const urlParams = useSearchParams()
     const hid = urlParams.get('id')
     const name = urlParams.get('name')
     const dispatch = useDispatch<AppDispatch>()
 
     const [bookingDate, setBookingDate] = useState<Dayjs|null>(null);
-    const [bookingHospital, setBookingHospital] = useState<string>("Chula");
-    const [bookingName, setBookingName] = useState<string>('');
-    const [bookingLastname, setBookingLastname] = useState<string>('');
-    const [citizenId, setCitizenId] = useState<string>('');
+  const [bookingHospital, setBookingHospital] = useState<string>("Chula");
+  const [bookingName, setBookingName] = useState<string>('');
+  const [bookingLastname, setBookingLastname] = useState<string>('');
+  const [citizenId, setCitizenId] = useState<string>('');
 
     const createBooking = () =>{
       if(bookingLastname&&citizenId&&bookingName&&bookingDate&&bookingHospital){
@@ -69,11 +75,33 @@ export default function Booking(){
         />
       </div>
         <div className="text-md text-left text-gray-600 pt-5">
-            Book Date and Hospital
+            Book Date and Companny
         </div>
         <DateReserve onDateChange={(value:Dayjs)=>{setBookingDate(value)}} onLocationChange={(value:string)=>{setBookingHospital(value)}}/>
     </div>
-    <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 shadow-sm text-white" onClick={createBooking}>Book Vaccine</button>
+    <button
+  className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 shadow-sm text-white"
+  onClick={() => {
+    if (!bookingDate || bookingDate.isBefore(startDate) || bookingDate.isAfter(endDate)) {
+      alert('Please select a date between May 10th  and May 13th, 2022.');
+      return; // Exit if the selected date is not within the specified range
+    }
+   
+    if (hospitalItems.length >= 3) {
+      alert('Cannot book more than three appointments');
+      return;
+    }
+  
+    else{
+   
+      createBooking();
+    }
+    
+  }}
+>
+  Book Appointment
+</button>
+
 
 </main>
 
