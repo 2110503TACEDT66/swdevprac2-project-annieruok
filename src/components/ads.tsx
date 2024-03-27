4bf54166a1e9a3d3d32069430ac9757cf0392559
Nawat Pimolsathien <nawatpim@gmail.com>
2024-03-27 14:23:07 +0700

    deploy02 fix path

--- src/components/ads.tsx
+++ src/components/ads.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/close'
+import CloseIcon from '@mui/icons-material/Close'
 
 const imageList = [
   '/img/b1.jpg',
